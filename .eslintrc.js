d0a919a246d860ef6e5f756d4f857a38587e86d2
praburangki <praburangki@gmail.com>
2018-07-10 15:56:07 +0800

    added vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,15 @@ module.exports = {
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#bulb-rules
-    'plugin:vue/recommended',
+    'plugin:vue/strongly-recommended',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard',
     // https://github.com/prettier/eslint-config-prettier
     'prettier',
     'prettier/standard',
+  ],
+  plugins: [
+    'prettier'
   ],
   rules: {
     // Only allow debugger in development
@@ -19,6 +22,7 @@ module.exports = {
     'no-console': process.env.PRE_COMMIT
       ? ['error', { allow: ['warn', 'error'] }]
       : 'off',
+    'prettier/prettier': 'error'
   },
   overrides: [
     {
--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
-import Vue from 'vue'
-import App from './app'
-import router from '@router'
-import store from '@state/store'
-import '@components/_globals'
+import Vue from 'vue';
+// import './plugins/vuetify';
+import App from './app';
+import router from '@router';
+import store from '@state/store';
+import '@components/_globals';
 
 // Don't warn about using the dev version of Vue in development
-Vue.config.productionTip = process.env.NODE_ENV === 'production'
+Vue.config.productionTip = process.env.NODE_ENV === 'production';
 
 const app = new Vue({
   router,
   store,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
 
 // If running inside Cypress
 if (window.Cypress) {
   // Attach the app to the window, which can be useful
   // for manually setting state in Cypress commands
   // such as `cy.logIn()`
-  window.__app__ = app
+  window.__app__ = app;
 }
