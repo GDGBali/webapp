a80901b30a42bef9f1398f8a449f043532820586
praburangki <praburangki@gmail.com>
2018-08-14 15:57:49 +0800

    :bug: - :gear: - adding navigation route to workbox

--- dev/service-worker.js
+++ dev/service-worker.js
@@ -1,13 +1,8 @@
 /* eslint-disable no-undef */
 
-workbox.routing.registerRoute(
-  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'google-fonts',
-  })
-);
-
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerNavigationRoute('/index.html');
 
 self.addEventListener('message', event => {
   if (!event.data) return;
