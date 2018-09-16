0c00ad811d7edda56714e42255291eb7824bbdf6
praburangki <praburangki@gmail.com>
2018-09-16 15:41:07 +0800

    :recycle: - Align footer items

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,38 +1,39 @@
 <template>
   <v-footer height="auto" class="footer">
     <v-container grid-list-xl fluid>
-      <v-layout align-center>
+      <v-layout align-center wrap>
         <v-flex
-          xs5
-          sm4
-          md3
+          xs4
+          md2
         >
           <GdgIcon />
         </v-flex>
-      </v-layout>
-      <v-layout
-        row
-        wrap
-        align-center
-        justify-space-between
-      >
-        <v-flex xs12 class="text-xs-right">
-          <a
-            v-for="socialLink in socialLinks"
-            :key="socialLink.icon"
-            :href="socialLink.link"
+        <v-flex xs8 md10>
+          <v-layout
+            row
+            wrap
+            align-center
+            justify-space-between
           >
-            <img
-              class="lazyload"
-              :data-src="require(`@assets/icons/${socialLink.icon}.svg`)"
-              :alt="`icon-${socialLink.icon}`"
-            >
-          </a>
-        </v-flex>
-        <v-flex xs12>
-          <div class="caption text-xs-right">
-            Designed & Develop by GDG Bali.
-          </div>
+            <v-flex xs12 class="text-xs-right">
+              <a
+                v-for="socialLink in socialLinks"
+                :key="socialLink.icon"
+                :href="socialLink.link"
+              >
+                <img
+                  class="lazyload"
+                  :data-src="require(`@assets/icons/${socialLink.icon}.svg`)"
+                  :alt="`icon-${socialLink.icon}`"
+                >
+              </a>
+            </v-flex>
+            <v-flex xs12>
+              <div class="caption text-xs-right">
+                Designed & Develop by GDG Bali.
+              </div>
+            </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
