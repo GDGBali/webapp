1b1b3443d4ec8c78bca45d58553fbd3dfcbd7a30
praburangki <praburangki@gmail.com>
2018-09-17 14:33:46 +0800

    :recycle: - :construction: - #7 Move event details to a component

---
+++ src/components/Events/EventDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout wrap justify-center>
+    <v-flex xs12 sm4>
+      <v-img
+        class="lazyload"
+        :src="event.coverImgUrl"
+      />
+    </v-flex>
+    <v-flex xs12 sm9>
+      <v-layout wrap>
+        <FabOrange icon="event" content="22 July 2018" />
+        <FabOrange icon="directions" content="Kembali Innovation Hub" />
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import FabOrange from './FabOrange';
+export default {
+  components: {
+    FabOrange,
+  },
+  props: {
+    event: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
---
+++ src/components/Events/FabOrange.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout :column="isColumn" align-center class="title flex sm6 xs12">
+    <v-btn 
+      fab 
+      dark 
+      small 
+      color="orange accent-4"
+      :class="{'mb-3': isColumn }"
+    >
+      <v-icon>{{ icon }}</v-icon>
+    </v-btn>
+    {{ content }}
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    isColumn() {
+      return !this.$vuetify.breakpoint.xsOnly;
+    },
+  },
+};
+</script>
