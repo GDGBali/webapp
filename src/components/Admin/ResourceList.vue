ef51e7a1aef012214b199310013989531d53f95d
praburangki <praburangki@gmail.com>
2018-10-07 17:55:03 +0800

    :recycle: - Refactor index resources page to a component

---
+++ src/components/Admin/ResourceList.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout justify-center>
+    <div class="display-1">
+      {{ title }}
+    </div>
+    <div>
+      <v-btn
+        fixed
+        dark
+        fab
+        bottom
+        right
+        color="secondary"
+        class="addBtn"
+        :to="addHref"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+    </div>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    addHref: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.addBtn {
+  right: 50px;
+  bottom: 50px;
+}
+</style>
--- src/router/views/admin/events/index.vue
+++ src/router/views/admin/events/index.vue
@@ -1,39 +1,17 @@
 <template>
-  <v-layout justify-center>
-    <div class="display-1">
-      Events
-    </div>
-    <div>
-      <v-btn
-        fixed
-        dark
-        fab
-        bottom
-        right
-        color="secondary"
-        class="addBtn"
-        to="/kelian/events/new"
-      >
-        <v-icon>add</v-icon>
-      </v-btn>
-    </div>
-  </v-layout>
+  <ResourceList title="Events" add-href="/kelian/events/new" />
 </template>
 
 <script>
 import { setPageTitle } from '@utils/adminPage';
+import ResourceList from '@components/Admin/ResourceList';
 
 export default {
+  components: {
+    ResourceList,
+  },
   created() {
     setPageTitle(this, 'Events');
   },
 };
 </script>
-
-
-<style lang="stylus" scoped>
-.addBtn {
-  right: 50px;
-  bottom: 50px;
-}
-</style>
