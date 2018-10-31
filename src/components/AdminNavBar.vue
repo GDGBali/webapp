e2a2e5366b45a7bcc103086f95fd0c29367d413a
praburangki <praburangki@gmail.com>
2018-10-31 14:43:37 +0800

    :recycle: - separate admin navbar component

---
+++ src/components/AdminNavBar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-navigation-drawer 
+      v-model="drawer"
+      persistent 
+      enable-resize-watcher
+      dark
+      app
+    >
+      <div class="pa-3 text-xs-center">
+        <div class="display-2 py-4">
+          GDG Bali Admin
+        </div>
+      </div>
+      <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <v-img :src="currentUser.avatarUrl" class="primary" />
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ currentUser.name }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-divider />
+      <v-list dense="dense">
+        <template v-if="$can('manage', 'all')">
+          <template v-for="(item) in menus">
+            <v-list-tile
+              :to="`/kelian${item.href}`" 
+              router="router" 
+              ripple="ripple"
+              :key="item.title"
+              exact
+            >
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar 
+      class="primary darken-1" 
+      fixed
+      dark
+      app
+    >
+      <v-toolbar-side-icon dark @click.native.stop="drawer = !drawer" />
+      <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
+      <v-spacer />
+      <v-btn flat to="/">
+        home
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import adminItems from '@src/data/adminItems';
+import { mapState } from 'vuex';
+
+import { authComputed } from '@state/helpers';
+
+export default {
+  data: () => ({
+    drawer: true,
+    menus: [],
+  }),
+  mounted() {
+    this.menus = adminItems;
+  },
+  computed: {
+    ...mapState('admin', ['pageTitle']),
+    ...authComputed,
+  },
+};
+</script>
--- src/router/layouts/admin.vue
+++ src/router/layouts/admin.vue
@@ -1,69 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer 
-      v-model="drawer" 
-      :mini-variant.sync="mini" 
-      persistent 
-      enable-resize-watcher
-      :dark="dark"
-      app
-    >
-      <div class="pa-3 text-xs-center" v-show="!mini">
-        <div class="display-2 py-4">
-          GDG Bali Admin
-        </div>
-      </div>
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <v-img :src="currentUser.avatarUrl" class="primary" />
-            </v-list-tile-avatar>
-
-            <v-list-tile-content>
-              <v-list-tile-title>{{ currentUser.name }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-      <v-divider />
-      <div v-if="$can('manage', 'all')">
-        <h1>hello</h1>
-      </div>
-      <v-list dense="dense">
-        <template v-if="$can('manage', 'all')">
-          <template v-for="(item) in menus">
-            <v-list-tile
-              :to="`/kelian${item.href}`" 
-              router="router" 
-              ripple="ripple"
-              :key="item.title"
-              exact
-            >
-              <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </template>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar 
-      class="primary darken-1" 
-      fixed="fixed" 
-      dark="dark"
-      app
-    >
-      <v-toolbar-side-icon dark="dark" @click.native.stop="drawer = !drawer" />
-      <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
-      <v-spacer />
-      <v-btn flat to="/">
-        home
-      </v-btn>
-    </v-toolbar>
+    <AdminNavBar />
     <v-content>
       <slot />
     </v-content>
@@ -84,10 +21,14 @@ const AuthDialog = () =>
 const BaseSnackbar = () =>
   import(/* webpackChunkName: "navbar" */ '@components/Base/BaseSnackbar');
 
+const AdminNavBar = () =>
+  import(/* webpackChunkName: "navbar" */ '@components/AdminNavBar');
+
 export default {
   components: {
     AuthDialog,
     BaseSnackbar,
+    AdminNavBar,
   },
   data: () => ({
     dark: false,
